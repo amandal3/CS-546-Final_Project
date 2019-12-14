282f2481fd1b37be46764174e9d2c211525754a2
yklhan <45218606+yklhan@users.noreply.github.com>
2019-12-14 10:38:34 +0800

    signUp routes added

--- routes/signUp.js
+++ routes/signUp.js
@@ -1,10 +1,29 @@
 var express = require("express");
-
+const userData = require("../data/users");
+const bcrypt = require("bcryptjs");
+const saltRounds = 16
 var router = express.Router();
 
 /* GET users listing. */
 router.get("/", function(req, res, next) {
-  res.send("respond with a resource");
+  res.render("register");
+});
+
+router.post("/", async function(req, res, next) {
+  try {
+    console.log(req.body.username);
+    const hash = await bcrypt.hash(req.body.password, saltRounds);
+    const result = await userData.create(
+      req.body.firstName,
+      req.body.lastName,
+      req.body.username,
+      hash,
+    );
+    console.log(result);
+    res.redirect("/login");
+  } catch (e) {
+    console.log(e);
+  }
 });
 
 module.exports = router;
