f96a734bf95790d152d0661626b4dd8707c2fcaa
zhengluyun <56028800+zhengluyun@users.noreply.github.com>
2019-12-13 22:31:28 -0500

    Update login.js

--- routes/login.js
+++ routes/login.js
@@ -4,7 +4,8 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get("/", function(req, res, next) {
-  res.send("respond with a resource");
+  res.render("login");
 });
 
 module.exports = router;
+
