ebb476d01a55c18ac2e569aba272e13877523e22
yklhan <45218606+yklhan@users.noreply.github.com>
2019-12-15 12:32:52 +0800

    adapte for new added 'user' data collection

--- routes/expense.js
+++ routes/expense.js
@@ -2,11 +2,27 @@ const express = require("express");
 
 const router = express.Router();
 const expenseData = require("../data/expenses");
+const userData = require("../data/users");
 
 /* GET users listing. */
 router.get("/", async function(req, res, next) {
   try {
-    const allExpenses = await expenseData.getAll();
+    if(!req.session.user){
+      res.render("login");
+      return;
+    }
+    let user = req.session.user;
+    let onetimeexpenses = user.expenses;
+    let rexpenses = user.recurringExpenses;
+    let allExpenses = [];
+    for(i=0; i < onetimeexpenses.length; i++){
+      let oneonetemp = await expenseData.get(onetimeexpenses[i].toString());
+      allExpenses.push(oneonetemp);
+    }
+    for(i=0; i < rexpenses.length; i++){
+      let onetemp = await expenseData.get(rexpenses[i].toString());
+      allExpenses.push(onetemp);
+    }
     // console.log(allExpenses);
     res.render("table", { allExpenses });
   } catch (e) {
@@ -19,13 +35,19 @@ router.get("/addExpense", function(req, res, next) {
 });
 router.post("/addExpense", async function(req, res, next) {
   try {
+    if(!req.session.user){
+      res.render("login");
+      return;
+    }
     const result = await expenseData.create(
       req.body.name,
       req.body.category,
       parseFloat(req.body.amount),
       req.body.comment,
-      0
+      5
     );
+    console.log(result);
+    let usertempresult = userData.addExp(result[1].toString(), req.session.user._id.toString(), 5);
     // console.log(result);
     res.redirect("/");
   } catch (e) {
