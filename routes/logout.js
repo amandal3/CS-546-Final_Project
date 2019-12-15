db4055ca94d917535a9c5cb614c22f6742ca798a
yklhan <45218606+yklhan@users.noreply.github.com>
2019-12-15 09:06:50 +0800

    update log out

--- routes/logout.js
+++ routes/logout.js
@@ -3,8 +3,13 @@ var express = require("express");
 var router = express.Router();
 
 /* GET users listing. */
-router.get("/", function(req, res, next) {
-  res.send("respond with a resource");
-});
 
+router.get('/', function(req, res){
+  if(req.session.user){
+      req.session.destroy();
+      res.render('logout');
+  }else{
+      res.render('error');
+  }
+});
 module.exports = router;
